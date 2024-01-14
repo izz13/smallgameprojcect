5bcee8eb398c6728ceee093a3c520f346e054b4c
windfall2718 <windfall2718@gmail.com>
2024-01-14 09:48:09 +0900

    Update Mover.cs
    made the player move left and right using Input.GetAxis

--- Scripts/Mover.cs
+++ Scripts/Mover.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     internal Rigidbody rb;
+    public float horizontal, vertical;
+    public float speed = 5f;
+    public bool moving = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,24 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        Move(direction);
+    }
+
+    void Move(Vector3 direction)
+    {
+        if (direction != Vector3.zero)
+        {
+            direction.Normalize();
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+        direction.y = rb.velocity.y;
+        rb.velocity = direction * speed;
     }
 }
