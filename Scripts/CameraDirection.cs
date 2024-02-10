967d64e15b2610831e03946127edffd0a75ca1c6
windfall2718 <windfall2718@gmail.com>
2024-02-11 08:47:15 +0900

    annotated Mover and CameraDirection script

--- Scripts/CameraDirection.cs
+++ Scripts/CameraDirection.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class CameraDirection : MonoBehaviour
 {
-    [SerializeField]
-    Transform player;
+    [SerializeField] // allows the variable to get the value from Unity
+    Transform player; // get the Transform value of the player
 
     [SerializeField]
-    float distance = 15f, height = 15f;
+    float distance = 15f, height = 15f; // create new float variables distance and height, and those have defalut value of 15f.
 
-    void FindPlayer()
+    void FindPlayer() // method that adjust the camera position
     {
-        Vector3 cameraPosition = new Vector3(player.position.x, player.position.y + height, player.position.z - distance);
-        transform.position = cameraPosition;
-        Vector3 cameraDirection = player.position - transform.position;
-        transform.rotation = Quaternion.LookRotation(cameraDirection, Vector3.up);
+        Vector3 cameraPosition = new Vector3(player.position.x, player.position.y + height, player.position.z - distance); // declare vector 3 variable for the camera position, which stores value that is calculated based on the player position. 
+        transform.position = cameraPosition; // set the position of the camera to the cameraPosition value
+        Vector3 cameraDirection = player.position - transform.position; // declare vector3 variable which stores direction of the camera
+        transform.rotation = Quaternion.LookRotation(cameraDirection, Vector3.up); //rotate the camera according to the value in the camera direction. Quaternion.LookRotation: returns a rotation that is alligned towards the camera direction.
     }
 
-    private void Update()
+    private void Update() // method that update the cameradirection every frame
     {
-        FindPlayer();
+        FindPlayer(); // call findplayer method
     }
 }
--- Scripts/Mover.cs
+++ Scripts/Mover.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    internal Rigidbody rb;
-    public float horizontal, vertical;
-    public float speed = 5f;
-    public bool moving = false;
-    Vector3 startPosition = Vector3.zero;
-    public Vector3 direction;
-    public Vector3 velocity;
+    internal Rigidbody rb; //declare rigid body object
+    public float horizontal, vertical; // declare variables which store horizontal and vertical value
+    public float speed = 5f; // set speed 
+    public bool moving = false; // declare variable that tracks movement of the body
+    Vector3 startPosition = Vector3.zero; // set start position as (0, 0, 0)
+    public Vector3 direction; //declare variable stores x, y, z vector values of direction
+    public Vector3 velocity; //declare variable stores x, y, z vector values of velocity
 
-    public void PlayerStart()
+    public void PlayerStart() // method which operates when the game starts
     {
-        transform.position = startPosition;
-        rb = GetComponent<Rigidbody>();
+        transform.position = startPosition; // set the position as the start position which is (0,0,0)
+        rb = GetComponent<Rigidbody>(); //set the main body object as the rigid body
     }
 
-    public void PlayerUpdate()
+    public void PlayerUpdate() // method which update the movement based on the user's input
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-        direction = new Vector3(horizontal, 0f, vertical);
-        Move(direction);
+        horizontal = Input.GetAxisRaw("Horizontal"); //set the horizontal value according to the input of the right and left arrow key
+        vertical = Input.GetAxisRaw("Vertical"); // set the vertical value according to the input of the up and down arrow key
+        direction = new Vector3(horizontal, 0f, vertical); // set the direction of the body according to the value stored in horizontal and vertical variable
+        Move(direction); // then call move function by giving direction as parameter (which makes movement to the object according to the value in the direction variable)
     }
 
-    void Move(Vector3 direction)
+    void Move(Vector3 direction) //method which move the object
     {
-        if (direction != Vector3.zero)
+        if (direction != Vector3.zero) // check whetehr there is value in the direction variable
         {
-            direction = direction.normalized;
-            moving = true;
+            direction = direction.normalized; // if there is value, then normalized the direction (normalized makes the object to move in the same speed regrardless the direction)
+            moving = true; // then trigger the moving variable to true
         }
-        else
+        else // if there is no value in direction variable, then
         {
-            moving = false;
+            moving = false; // trigger the moving variable to false
         }
-        velocity = new Vector3(direction.x * speed, rb.velocity.y, direction.z * speed);
-        rb.velocity = velocity;
+        velocity = new Vector3(direction.x * speed, rb.velocity.y, direction.z * speed); // calculate the velocity according to the value in the direction variable
+        rb.velocity = velocity; // update the rigid body velocity to the value in the velocity variable
     }
 }
